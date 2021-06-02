2d1a4cc9ee74f23e5b55ade614bdf6c323c38861
yUm1k4 <zaiabdullah99@gmail.com>
2021-06-03 06:44:22 +0700

    fix: bug scale cv

--- assets/js/main.js
+++ assets/js/main.js
@@ -126,5 +126,5 @@ resumeButton.addEventListener('click', () => {
     generateResume()
 
     // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
-    setTimeout(removeScale, 50000)
+    setTimeout(removeScale, 5000)
 })
